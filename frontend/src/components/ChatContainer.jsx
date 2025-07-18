0c90bfc23857c728d104d1f693985039093a1548
Akanksha-94 <akankshabhardwaj447@gmail.com>
2025-07-18 12:41:07 +0530

    updated

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
-import assets from '../assets/assets'
-import { messagesDummyData } from '../assets/assets'
+import React, {useEffect,useRef} from 'react'
+
+import assets,{ messagesDummyData } from '../assets/assets'
+import { formatMessageTime } from '../library/utils'
 
 const ChatContainer = ({selectedUser, setSelectedUser}) => {
+  
+  
+    const scrollEnd =useRef()
+    useEffect(()=>{
+          if(scrollEnd.current){
+            scrollEnd.current.scrollIntoView({ behavior: 'smooth' })
+          }
+    },[])
+  
   return selectedUser ? (
     <div className='h-full overflow-scroll relative backdrop-blur-lg'>
       
@@ -30,14 +40,32 @@ const ChatContainer = ({selectedUser, setSelectedUser}) => {
               <p className={`p-2 max-w-[200px] md:text-sm font-light rounded-lg mb-8 break-all bg-violet-500/30 text-white ${msg.senderId === '680f50e4f10f3cd28382ecf9' ? 'rounded-br-none' : 'rounded-bl-none'}`}>{msg.text}</p>
             )}
 
-            {/* <div className='text-center text-xs'>
+            <div className='text-center text-xs'>
               <img src={msg.senderId === '680f50e4f10f3cd28382ecf9' ? assets.avatar_icon : assets.profile_martin} alt="" className='w-7 rounded-full' />
-              <p className='text-gray-500'>{msg.createdAt}</p>
-            </div> */}
+              <p className='text-gray-500'>{ formatMessageTime(msg.createdAt)}</p>
+            </div>
 
           </div>
         ))}
+        <div ref={scrollEnd}>
+
+        </div>
       </div>
+
+      {/* bottom -area */}
+      <div className="absolute bottom-0 left-0 right-0 flex items-center gap-3 p-3">
+        <div className="flex-1 flex items-center bg-gray-100/12 px-3 rounded-full">
+          <input type="text" placeholder="Send a message"
+            className="flex-1 text-sm p-3 border-none rounded-lg outline-none text-white placeholder-gray-400" />
+          <input type="file" id="image" accept="image/png, image/jpeg" hidden />
+          <label htmlFor="image">
+            <img src={assets.gallery_icon} alt="" className="w-5 mr-2 cursor-pointer" />
+          </label>
+        </div>
+        <img src={assets.send_button} alt="" className="w-7 cursor-pointer" />
+      </div>
+
+
 
 
     </div>
